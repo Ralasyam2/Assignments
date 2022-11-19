fb9c8bbb1854035de69213c668257cd4fb83cc7f
Ranjith <Ranjithalasyam97@gmail.com>
2022-11-18 21:57:07 -0600

    ranjithFirstAssignment

---
+++ src/com/basics/Nameprint.java
@@ -0,0 +1,13 @@
+package com.basics;
+import java.util.List;
+public class Nameprint{
+
+List list=new List();
+    public static void main(String[] args) {
+        System.out.println("    *    *         *     *******  *   *      *     *     *");
+        System.out.println("   * *   *        * *    ****      * *      * *    * * * *");
+        System.out.println("  *****  *       *****       ***    *      *****   *  *  *");
+        System.out.println(" *     * *****  *     *  *******    *     *     *  *     *");
+    }
+
+}

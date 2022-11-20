4bbf8b1027afe1b8c973bde9d0be69813e44bc73
Ranjith <Ranjithalasyam97@gmail.com>
2022-11-20 12:53:58 -0600

    My First Commit

--- src/com/basics/Nameprint.java
+++ src/com/basics/Nameprint.java
@@ -1,8 +1,5 @@
 package com.basics;
-import java.util.List;
 public class Nameprint{
-
-List list=new List();
     public static void main(String[] args) {
         System.out.println("    *    *         *     *******  *   *      *     *     *");
         System.out.println("   * *   *        * *    ****      * *      * *    * * * *");
